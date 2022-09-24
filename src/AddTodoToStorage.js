88087a1869ddcb3f82f8ee913b1bb4bd539c22fb
giritlerdenag <abdurrahman.girit53@gmail.com>
2022-09-24 15:33:04 +0300

    first commit

---
+++ src/AddTodoToStorage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ListGroupItem } from 'reactstrap'
+
+export default function addTodo(todo) {
+
+  return (
+<ListGroupItem>{todo}</ListGroupItem>
+
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import {
+  Button,
+  Container,
+  Form,
+  Input,
+  InputGroup,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
+import AddTodoToStorage from "./AddTodoToStorage";
 
 function App() {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="add-container">
+        <Container className="todo-container">
+          <h1 style={{ textAlign: "center", margin: "1rem" }}>MY TODO APP</h1>
+          <Form>
+            <InputGroup>
+              <Input
+                id="todoInput"
+                placeholder="Bir Todo Giriniz..."
+
+                onChange={handleChange}
+              ></Input>
+              <Button
+                onClick={(e) => {
+                  const listGroup = document.getElementById("listGroup");
+                  const root = createRoot(listGroup);
+                  root.render(<ListGroupItem>{todo}</ListGroupItem>);
+
+                  e.preventDefault();
+                }}
+                type="submit"
+              >
+                Ekle
+              </Button>
+            </InputGroup>
+          </Form>
+        </Container>
+      </div>
+      <br />
+      <br />
+      <div>
+        <Container className="todo-container">
+          <ListGroup id="listGroup"></ListGroup>
+        </Container>
+      </div>
     </div>
   );
 }
