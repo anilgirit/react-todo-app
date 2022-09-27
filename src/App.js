c9a3564740e63c88bb6ae9dd657f2ab2291010d4
giritlerdenag <abdurrahman.girit53@gmail.com>
2022-09-27 17:09:11 +0300

    Calisan hali

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
 import "./App.css";
 import {
   Button,
@@ -10,14 +9,37 @@ import {
   ListGroup,
   ListGroupItem,
 } from "reactstrap";
-import AddTodoToStorage from "./AddTodoToStorage";
+import { MdDeleteForever } from "react-icons/md";
 
 function App() {
-  const [todo, setTodo] = useState("");
+  const [input, setInput] = useState("");
+
+  const [todos, setTodos] = useState([]);
 
   const handleChange = (event) => {
-    setTodo(event.target.value);
+    setInput(event.target.value);
   };
+
+  function addTodo(e) {
+    if (!input) {
+      alert("Bir Todo Giriniz");
+      return;
+    }
+
+    const todo = {
+      id: Math.floor(Math.random() * 1000),
+      value: input,
+    };
+    setTodos((oldTodos) => [...oldTodos, todo]);
+    setInput("");
+  }
+  function clearInput() {
+    document.getElementById("todoInput").value = "";
+  }
+  function deleteTodo(id) {
+    const newTodoArray = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodoArray);
+  }
   return (
     <div>
       <div className="add-container">
@@ -28,15 +50,12 @@ function App() {
               <Input
                 id="todoInput"
                 placeholder="Bir Todo Giriniz..."
-
                 onChange={handleChange}
               ></Input>
               <Button
                 onClick={(e) => {
-                  const listGroup = document.getElementById("listGroup");
-                  const root = createRoot(listGroup);
-                  root.render(<ListGroupItem>{todo}</ListGroupItem>);
-
+                  addTodo();
+                  clearInput();
                   e.preventDefault();
                 }}
                 type="submit"
@@ -51,7 +70,18 @@ function App() {
       <br />
       <div>
         <Container className="todo-container">
-          <ListGroup id="listGroup"></ListGroup>
+          <ListGroup>
+            {todos.map((todo) => {
+              return (
+                <ListGroupItem id="li" key={todo.id}>
+                  {todo.value}
+                  <Button onClick={() => deleteTodo(todo.id)} color="danger">
+                    <MdDeleteForever />
+                  </Button>
+                </ListGroupItem>
+              );
+            })}
+          </ListGroup>
         </Container>
       </div>
     </div>
